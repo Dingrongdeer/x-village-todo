0a82f32e14c2340af9986034381700e405b0f9f9
Dingrongdeer <40971620+Dingrongdeer@users.noreply.github.com>
2018-09-03 02:22:50 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# x-village-todo
-To-do List
+# X-Village Web Course Accounting System Example
+
+
+## Setup
+
+- Run python shell
+
+```sh
+python
+
+```
+
+```python
+>>> from app import db
+>>> db.create_all()
+>>> exit()
+```
+
+## Usage
+
+### Windows
+```sh
+set FLASK_APP=app.py
+flask run
+```
+
+### macOS/Linux
+```
+export FLASK_APP=app.py
+flask run
+```
+
+
+
+## Authors
+[Lee-W](https://github.com/Lee-W/)
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ views.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify, render_template
+
+from app import app, db
+from models import Record
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', title='To-do List')
+
+
+@app.route("/record", methods=['POST'])
+def add_record():
+    req_data = request.form
+    name = req_data['name']
+
+    record = Record(name=name)
+    db.session.add(record)
+    db.session.commit()
+    return 'Create Succeeded', 200
+
+
+@app.route("/record", methods=['GET'])
+def get_records():
+    records = Record.query.all()
+    records_data = [
+        {
+            'id': record.id,
+            'name': record.name
+        }
+        for record in records
+    ]
+    return jsonify(records_data), 200
+
+
+@app.route('/record/<int:record_id>', methods=['GET'])
+def get_record(record_id):
+    record = Record.query.filter_by(id=record_id).first()
+    record_data = {
+        'id': record.id,
+        'name': record.name,
+
+    }
+    return jsonify(record_data), 200
+
+
+@app.route('/record/<int:record_id>', methods=['PUT'])
+def update_record(record_id):
+    req_data = request.form
+    record = Record.query.filter_by(id=record_id).first()
+    record.name = req_data['name']
+    db.session.add(record)
+    db.session.commit()
+    return 'Update Succeeded', 200
+
+
+@app.route("/record/<int:record_id>", methods=["DELETE"])
+def delete_record(record_id):
+    record = Record.query.filter_by(id=record_id).first()
+    db.session.delete(record)
+    db.session.commit()
+    return 'Delete Succeeded', 200
